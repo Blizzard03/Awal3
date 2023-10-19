4e546b76402ad51e573dc9eba1272708bd10f30b
Blizzard03 <mariq.rasyid@gmail.com>
2023-10-19 17:31:08 +0700

    modify matakuliah.java and mainclass

--- src/awal3/Awal3/Main_Class/Main_Class.java
+++ src/awal3/Awal3/Main_Class/Main_Class.java
@@ -17,10 +17,41 @@ public class Main_Class {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Material 1
         Mata_Kuliah mataKuliah1 = new Mata_Kuliah("MK101", "Pemrograman Java", 4, 3.5, 'A');
         Mata_Kuliah mataKuliah2 = new Mata_Kuliah("MK202", "Basis Data", 3, 3.0, 'B');
         Mata_Kuliah mataKuliah3 = new Mata_Kuliah("MK303", "Sistem Operasi", 3, 3.2, 'C');
 
+        // Material 2
+        Mata_Kuliah mata_Kuliah4 = new Mata_Kuliah("CB-761", "Cyber Security", 4);
+        Mata_Kuliah mata_Kuliah5 = new Mata_Kuliah("HD-853", "Human Development", 8);
+        Mata_Kuliah mata_Kuliah6 = new Mata_Kuliah("WH-781", "Werewolf History", 14);
+
+        // Material 3
+        Mata_Kuliah mata_Kuliah7 = new Mata_Kuliah();
+        mata_Kuliah7.kode = "WP-851";
+        mata_Kuliah7.nama = "Werewolves Philosophy";
+        mata_Kuliah7.sks = 6;
+        mata_Kuliah7.bobotNilai = 3.8;
+        mata_Kuliah7.grade = 'A';
+
+        Mata_Kuliah mata_Kuliah8 = new Mata_Kuliah();
+        mata_Kuliah8.kode = "WE-391";
+        mata_Kuliah8.nama = "Wolf Ecology";
+        mata_Kuliah8.sks = 4;
+        mata_Kuliah8.bobotNilai = 3.1;
+        mata_Kuliah8.grade = 'B';
+
+        Mata_Kuliah mata_Kuliah9 = new Mata_Kuliah();
+        mata_Kuliah9.kode = "LP-563";
+        mata_Kuliah9.nama = "Lion Philosophy";
+        mata_Kuliah9.sks = 8;
+        mata_Kuliah9.bobotNilai = 3.51;
+        mata_Kuliah9.grade = 'A';
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Material 1");
+        System.out.println("-----------------------------------------------------------");
         System.out.println("Mata Kuliah 1:");
         System.out.println("Kode: " + mataKuliah1.getKode());
         System.out.println("Nama: " + mataKuliah1.getNama());
@@ -40,7 +71,54 @@ public class Main_Class {
         System.out.println("Nama: " + mataKuliah3.getNama());
         System.out.println("SKS: " + mataKuliah3.getSks());
         System.out.println("Bobot Nilai: " + mataKuliah3.getBobotNilai());
-        System.out.println("Grade: " + mataKuliah3.getGrade());
+        System.out.println("Grade: " + mataKuliah3.getGrade() + "\n");
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Material 2");
+        System.out.println("-----------------------------------------------------------");
+
+        System.out.println("\nMata Kuliah 4:");
+        System.out.println("Kode: " + mata_Kuliah4.getKode());
+        System.out.println("Nama: " + mata_Kuliah4.getNama());
+        System.out.println("SKS: " + mata_Kuliah4.getSks());
+
+        System.out.println("\nMata Kuliah 5:");
+        System.out.println("Kode: " + mata_Kuliah5.getKode());
+        System.out.println("Nama: " + mata_Kuliah5.getNama());
+        System.out.println("SKS: " + mata_Kuliah5.getSks());
+
+        System.out.println("\nMata Kuliah 6:");
+        System.out.println("Kode: " + mata_Kuliah6.getKode());
+        System.out.println("Nama: " + mata_Kuliah6.getNama());
+        System.out.println("SKS: " + mata_Kuliah6.getSks());
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Material 3");
+        System.out.println("-----------------------------------------------------------");
+
+        System.out.println("Mata Kuliah 7:");
+        System.out.println("Kode: " + mata_Kuliah7.getKode());
+        System.out.println("Nama: " + mata_Kuliah7.getNama());
+        System.out.println("SKS: " + mata_Kuliah7.getSks());
+        System.out.println("Bobot Nilai: " + mata_Kuliah7.getBobotNilai());
+        System.out.println("Grade: " + mata_Kuliah7.getGrade());
+
+        System.out.println("Mata Kuliah 8:");
+        System.out.println("Kode: " + mata_Kuliah8.getKode());
+        System.out.println("Nama: " + mata_Kuliah8.getNama());
+        System.out.println("SKS: " + mata_Kuliah8.getSks());
+        System.out.println("Bobot Nilai: " + mata_Kuliah8.getBobotNilai());
+        System.out.println("Grade: " + mata_Kuliah8.getGrade());
+
+        System.out.println("Mata Kuliah 9:");
+        System.out.println("Kode: " + mata_Kuliah9.getKode());
+        System.out.println("Nama: " + mata_Kuliah9.getNama());
+        System.out.println("SKS: " + mata_Kuliah7.getSks());
+        System.out.println("Bobot Nilai: " + mata_Kuliah7.getBobotNilai());
+        System.out.println("Grade: " + mata_Kuliah7.getGrade());
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Program Made by WPA TEAM");
+        System.out.println("-----------------------------------------------------------");
     }
-    
+
 }
--- src/awal3/Awal3/Mata_Kuliah/Mata_Kuliah.java
+++ src/awal3/Awal3/Mata_Kuliah/Mata_Kuliah.java
@@ -10,18 +10,34 @@ package awal3.Awal3.Mata_Kuliah;
  */
 public class Mata_Kuliah {
 
-    private String kode;
-    private String nama;
-    private int sks;
-    private double bobotNilai;
-    private char grade;
+    public String kode, nama;
+    public int sks;
+    public double bobotNilai;
+    public char grade;
 
+    // Full Constructor
     public Mata_Kuliah(String kode, String nama, int sks, double bobotNilai, char grade) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.bobotNilai = bobotNilai;
         this.grade = grade;
+    }
+
+    // Blank Constuctor
+    public Mata_Kuliah() {
+        this.kode = "Unknows";
+        this.nama = "Unknows";
+        this.sks = 0;
+        this.bobotNilai = 0.0;
+        this.grade = '-';
+    }
+
+    // Hanya Kode, Nama dan SKS
+    public Mata_Kuliah(String Code, String Name, int SKS) {
+        this.kode = Code;
+        this.nama = Name;
+        this.sks = SKS;
     }
 
     public String getKode() {
